dd9571a59a79c81873ac97e95cf9ab0c0ba5bdc6
kaneko takaaki <takaaki02k@icloud.com>
2023-07-23 20:36:47 +0900

    サンプルファイルの追加

---
+++ src/app/Http/Requests/ContactRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'building-name' => ['required', 'string', 'max:255'],
+        ];
+    }
+    public function messages()
+     {
+         return [
+             'name.required' => '名前を入力してください',
+             'name.string' => '名前を文字列で入力してください',
+             'name.max' => '名前を255文字以下で入力してください',
+             'email.required' => 'メールアドレスを入力してください',
+             'email.string' => 'メールアドレスを文字列で入力してください',
+             'email.email' => '有効なメールアドレス形式を入力してください',
+             'email.max' => 'メールアドレスを255文字以下で入力してください',
+             'address.required' => '住所を入力してください',
+            'address.string' => '住所を文字列で入力してください',
+            'address.max' => '住所を255文字以下で入力してください',
+             'building-name.required' => '建物名を入力してください',
+            'building-name.string' => '建物名を文字列で入力してください',
+            'building-name.max' => '建物名を255文字以下で入力してください',
+         ];
+     }
+}
